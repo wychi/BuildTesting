d886edda01416d9f46f028b9edeb9be73fb1560f
Wenyuan Chi <cwenyuan@gmail.com>
2015-05-29 02:28:01 +0800

    init

---
+++ mylibrary/src/main/java/com/example/wychi/mylibrary/MyClass.java
@@ -0,0 +1,14 @@
+package com.example.wychi.mylibrary;
+
+/**
+ * Created by wychi on 2015/5/17.
+ */
+public class MyClass {
+    public void func1() {
+        func2();
+    }
+
+    public void func2() {
+
+    }
+}
